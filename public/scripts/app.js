1ee2ad3a40d8594481e1e80a5d5a6e75b12ac0ea
atousley <annette.tousley@gmail.com>
2016-02-25 08:42:00 -0600

    tried adding some logic with a controller, not working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,20 +4,20 @@ myApp.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
         .when('/home', {
-            templateUrl: '/views/templates/home.html',
-            controller: 'HomeController'
+            templateUrl: '/views/templates/home.html'
         })
         .when('/leslie', {
-            templateUrl: '/views/templates/leslie.html',
+            templateUrl: '/views/templates/leslie.html'
         })
         .when('/broadcity', {
-            templateUrl: '/views/templates/broadcity.html',
+            templateUrl: '/views/templates/broadcity.html'
         })
         .when('/daria', {
-            templateUrl: '/views/templates/daria.html',
+            templateUrl: '/views/templates/daria.html'
         })
         .when('/serenablair', {
             templateUrl: '/views/templates/serenablair.html',
+            controller: 'SerenablairController'
         })
         .otherwise({
             redirectTo: 'home'
---
+++ public/scripts/controllers/serenablairController.js
@@ -0,0 +1,7 @@
+myApp.controller('SerenablairController', ['$scope', function($scope) {
+
+    $scope.showPhoto = function {
+        $scope.image = "../../images/serenablair.gif";
+    }
+
+}]);
